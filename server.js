90e1b5fd799e5ca935fde57f07a65d27173c5a91
Taylor Sanders <tsanders3515@gmail.com>
2021-04-19 18:19:50 -0800

    push for la

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const db = require("../models")
 
 //START A SERVER AND LISTEN ON PORT 3000
 const PORT = process.env.PORT || 3000
@@ -14,4 +16,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness_db", {
     useNewUrlParser: true,
     useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
 });
